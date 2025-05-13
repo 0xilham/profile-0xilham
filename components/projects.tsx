2f74ebdb9b459f59a9c4b4f832fc7dbea6e46f95
0xilham <ilhamhermawan@hotmail.com>
2025-05-13 19:43:36 +0700

    feat: update project tags for improved accuracy and relevance

--- components/projects.tsx
+++ components/projects.tsx
@@ -24,7 +24,7 @@ export default function Projects() {
       description:
         "A decentralized application focused on climate change awareness and action, built using Web3 technologies.",
       image: "https://hamhrmwn.notion.site/image/attachment%3Afbc32c0c-66bc-4129-83b2-c4101019d5f7%3ACuplikan_layar_2025-04-22_012404.png?table=block&id=1dc3d31e-8e12-80e3-b77e-d9e85579c106&spaceId=75df719b-aa8c-4e8b-9652-96bf850c35ee&width=2000&userId=&cache=v2",
-      tags: ["Web3","Node.js", "Next.js", "Solidity", "Ethereum", "HTML", "Tailwind CSS"],
+      tags: ["JavasScript", "Solidity", "Web3", "Node.js", "Next.js", "SmartContract", "EVM", "HTML & CSS", "Tailwind CSS"],
       demoLink: "https://blockchain-bank-recycle-waste.vercel.app/",
       codeLink: "https://github.com/0xilham/blockchain-bank-recycle-waste",
       category: "web",
@@ -33,7 +33,7 @@ export default function Projects() {
       title: "Optimization Rainfall Prediction Model - AI",
       description: "An AI model using Long Short-Term Memory (LSTM) to improve rainfall prediction accuracy.",
       image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/0*RdrAGOU-oD33glk7",
-      tags: ["AI", "LSTM", "Python", "Data Science", "Weather Forecasting"],
+      tags: ["AI", "LSTM", "Python", "Deep Learning", "Weather Forecasting"],
       demoLink: "https://medikom.iocspublisher.org/index.php/JTI/article/view/942",
       codeLink: "https://medikom.iocspublisher.org/index.php/JTI/article/view/942/87",
       category: "ai",
@@ -42,7 +42,7 @@ export default function Projects() {
       title: "ADAB: The Sins Of Trash - Game 2D",
       description: "A 2D adventure game developed with Unity, featuring dynamic gameplay and immersive storytelling.",
       image: "https://hamhrmwn.notion.site/image/attachment%3A7263b4d3-ea05-4cc0-8f54-665f4c087e68%3AADAB.jpg?table=block&id=1dc3d31e-8e12-8193-b3f6-cab124de0de8&spaceId=75df719b-aa8c-4e8b-9652-96bf850c35ee&width=2000&userId=&cache=v2",
-      tags: ["Unity", "C#", "Game Development", "2D Graphics", "Animation"],
+      tags: ["Unity", "C#", "Game Programming", "Game Development", "2D Graphics", "Animation"],
       demoLink: "https://rawrusstudio.itch.io/adab-thesinsoftrash",
       codeLink: "https://github.com/Game-Dev-Group-6/GDK6",
       category: "game",
@@ -52,7 +52,7 @@ export default function Projects() {
       description:
         "Purrfect Donations project a Web3 crowdfunding platform designed to enable cat food donations with transparency and blockchain-based security.",
       image: "https://hamhrmwn.notion.site/image/attachment%3Aa2ff1a92-e2ea-4160-9f40-3715f212eaa3%3ACuplikan_layar_2025-04-07_145723.png?table=block&id=1dc3d31e-8e12-8161-ab18-e787d35fb751&spaceId=75df719b-aa8c-4e8b-9652-96bf850c35ee&width=2000&userId=&cache=v2",
-      tags: ["Next.js", "Node.js", "MongoDB", "Stripe", "Tailwind CSS"],
+      tags: ["JavaScript", "Next.js", "Node.js", "EVM", "SmartContract", "Cryptocurrency", "HTML & CSS", "Tailwind CSS"],
       demoLink: "https://feed-donation.vercel.app/",
       codeLink: "https://github.com/0xilham/feed-donation-app",
       category: "web",
@@ -61,7 +61,7 @@ export default function Projects() {
       title: "Catatan Olah TKP - Mobile App",
       description: "Note-taking application for crime scene investigation, designed to assist investigators in documenting evidence.",
       image: "https://images.unsplash.com/photo-1637942695886-69c5094fb1bb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      tags: ["Java", "Android", "SQLite", "API Integration", "GPS"],
+      tags: ["Java", "Android", "SQLite", "GPS"],
       demoLink: "#",
       codeLink: "#",
       category: "mobile",
@@ -150,10 +150,10 @@ export default function Projects() {
 
         <div className="text-center mt-12">
           <Button>
-            <Link 
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/0xilham" className="flex items-center">
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/0xilham" className="flex items-center">
               <Code className="h-4 w-4 mr-2" /> View All Projects
             </Link>
           </Button>
