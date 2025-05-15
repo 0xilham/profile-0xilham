3c395c6d17685bee07a3424982d069b22042086b
0xilham <ilhamhermawan@hotmail.com>
2025-05-15 12:36:01 +0700

    feat: enhance button styling in mode toggle for improved accessibility and consistency

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -16,9 +16,13 @@ export function ModeToggle({ className }: ModeToggleProps) {
   }
 
   return (
-    <Button size="icon" className={`focus-visible:ring-0 ${className}`} onClick={toggleTheme}>
+    <Button
+      size="icon"
+      className={`focus-visible:ring-0 focus-visible:outline-none ${className} dark:bg-gray-700 dark:text-white`}
+      onClick={toggleTheme}
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-yellow-400" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
